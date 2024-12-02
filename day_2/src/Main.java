2ecd2eaf1e6470c0e33cbac4d0fcc4b556ed9711
chrstncrrnd <chrstncrrnd@gmail.com>
2024-12-02 20:08:43 +0100

    added missing java main file

---
+++ day_2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Part one:");
+        PartOne.main();
+        System.out.println("Part two:");
+        PartTwo.main();
+    }
+}
