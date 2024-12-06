49fc5cf66a13a06b47ca0130bd466cdef298ef8c
chrstncrrnd <chrstncrrnd@gmail.com>
2024-12-06 18:32:16 +0100

    formatted code

--- day_6/part_two/part_two.go
+++ day_6/part_two/part_two.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 )
 
-
 func readFile(name string) []string {
 	var out []string
 	file, err := os.Open(name)
@@ -25,8 +24,8 @@ func readFile(name string) []string {
 		out = append(out, text)
 	}
 
-  if err := scanner.Err(); err != nil {
-log.Fatal("Something went wrong with scanner: ", err)
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Something went wrong with scanner: ", err)
 	}
 
 	return out
@@ -36,12 +35,12 @@ var width int = -1
 var height int = -1
 
 func inArea(lines []string, x, y int) bool {
-	if width == -1{
-    width = len((lines)[0])
-  }
-  if height == -1 {
-    height = len(lines) 
-  }
+	if width == -1 {
+		width = len((lines)[0])
+	}
+	if height == -1 {
+		height = len(lines)
+	}
 	return x >= 0 && y >= 0 && y < height && x < width
 }
 
@@ -57,56 +56,55 @@ func startingPosition(lines []string) (int, int) {
 }
 
 func testMap(lines []string) bool {
-  var timesSeen map[string]int = make(map[string]int)
-  looped := false
-  traverse(lines, func(x, y int, char string, direction complex64) bool {
-    pos := encodePos(x, y)
-    timesSeen[pos] += 1
-    // 10 is arbitrary but a smaller number isn't that much faster and 10 seemed big enough lol
-    if timesSeen[pos] >= 10{
-      looped = true
-      return false;
-    }
-    return true; 
-  })
-  return looped
+	var timesSeen map[string]int = make(map[string]int)
+	looped := false
+	traverse(lines, func(x, y int, char string, direction complex64) bool {
+		pos := encodePos(x, y)
+		timesSeen[pos] += 1
+		// 10 is arbitrary but a smaller number isn't that much faster and 10 seemed big enough lol
+		if timesSeen[pos] >= 10 {
+			looped = true
+			return false
+		}
+		return true
+	})
+	return looped
 }
 
-func addObstacle(lines []string, x, y int) (out []string){
-  for cy, line := range lines {
-    newLine := ""
-    for cx, ch := range line {
-      var char string = string(ch)
-      if cx == x && cy == y {
-        char = "#"
-      }
-      newLine = newLine + char
-    }
-    out = append(out, newLine)
-  }
-  return 
+func addObstacle(lines []string, x, y int) (out []string) {
+	for cy, line := range lines {
+		newLine := ""
+		for cx, ch := range line {
+			var char string = string(ch)
+			if cx == x && cy == y {
+				char = "#"
+			}
+			newLine = newLine + char
+		}
+		out = append(out, newLine)
+	}
+	return
 }
 
-func allVisitedPositions(lines []string) (visitedPositions map[string]bool){
-  visitedPositions = make(map[string]bool);
-  traverse(lines, func(x, y int, char string, _ complex64) bool {
-    pos := encodePos(x, y)
-    visitedPositions[pos] = true
-    return true
-  })
-  return
+func allVisitedPositions(lines []string) (visitedPositions map[string]bool) {
+	visitedPositions = make(map[string]bool)
+	traverse(lines, func(x, y int, char string, _ complex64) bool {
+		pos := encodePos(x, y)
+		visitedPositions[pos] = true
+		return true
+	})
+	return
 }
-
 
 func traverse(lines []string, atPoint func(x, y int, char string, direction complex64) bool) {
-  direction := complex(0, -1)
-  x, y := startingPosition(lines)
-  for {
-    cont := atPoint(x, y, string(lines[y][x]), complex64(direction))
-    if !cont{
-      break;
-    }
-    newX, newY := x+int(real(direction)), y+int(imag(direction))
+	direction := complex(0, -1)
+	x, y := startingPosition(lines)
+	for {
+		cont := atPoint(x, y, string(lines[y][x]), complex64(direction))
+		if !cont {
+			break
+		}
+		newX, newY := x+int(real(direction)), y+int(imag(direction))
 		if !inArea(lines, newX, newY) {
 			break
 		}
@@ -120,28 +118,28 @@ func traverse(lines []string, atPoint func(x, y int, char string, direction comp
 	}
 }
 
-func encodePos(x, y int) (out string){
-  out = strconv.Itoa(x) + "," + strconv.Itoa(y)
-  return 
+func encodePos(x, y int) (out string) {
+	out = strconv.Itoa(x) + "," + strconv.Itoa(y)
+	return
 }
 
 func decodePos(pos string) (x, y int) {
-  temp := strings.Split(pos, ",")
-  x, _ = strconv.Atoi(temp[0])
-  y, _ = strconv.Atoi(temp[1])
-  return
+	temp := strings.Split(pos, ",")
+	x, _ = strconv.Atoi(temp[0])
+	y, _ = strconv.Atoi(temp[1])
+	return
 }
 
-func PartTwo(){
-  lines := readFile("input.txt")
-  possiblePlaces := allVisitedPositions(lines);
-  var total int
-  for pos, _ := range possiblePlaces {
-    x, y := decodePos(pos)
-    newLines := addObstacle(lines, x, y)
-    if testMap(newLines){
-      total ++;
-    }
-  }
-  fmt.Println("Total: ", total)
+func PartTwo() {
+	lines := readFile("input.txt")
+	possiblePlaces := allVisitedPositions(lines)
+	var total int
+	for pos, _ := range possiblePlaces {
+		x, y := decodePos(pos)
+		newLines := addObstacle(lines, x, y)
+		if testMap(newLines) {
+			total++
+		}
+	}
+	fmt.Println("Total: ", total)
 }
