d0076b7791f51e374f381b58509eecc01c023bc8
chrstncrrnd <chrstncrrnd@gmail.com>
2024-12-06 13:53:48 +0100

    formatted code

--- day_6/part_one.go
+++ day_6/part_one.go
@@ -1,83 +1,81 @@
-package main;
+package main
 
 import (
-  "fmt"
-  "os"
-  "log"
-  "bufio"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 )
 
-
 func readFile(name string) []string {
-  var out []string
-  file, err := os.Open(name);
-  if err != nil {
-    log.Fatal("Something went wrong with opening file: ", err)
-    return []string{}
-  }
-  defer file.Close()
+	var out []string
+	file, err := os.Open(name)
+	if err != nil {
+		log.Fatal("Something went wrong with opening file: ", err)
+		return []string{}
+	}
+	defer file.Close()
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan(){
-    text := scanner.Text()
-    out = append(out, text)
-  }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		out = append(out, text)
+	}
 
-  if err := scanner.Err(); err != nil{
-    log.Fatal("Something went wrong with scanner: ", err)
-  }
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Something went wrong with scanner: ", err)
+	}
 
-  return out
+	return out
 }
 
 func inArea(lines *[]string, x, y int) bool {
-  width := len((*lines)[0])
-  height := len(*lines)
-  return x >= 0 && y >= 0 && y < height && x < width
+	width := len((*lines)[0])
+	height := len(*lines)
+	return x >= 0 && y >= 0 && y < height && x < width
 }
 
 func startingPosition(lines []string) (int, int) {
-  for y, line := range lines {
-    for x, ch := range line {
-      if string(ch)== "^"{
-        return x, y
-      }
-    }
-  }
-  return 0, 0 
+	for y, line := range lines {
+		for x, ch := range line {
+			if string(ch) == "^" {
+				return x, y
+			}
+		}
+	}
+	return 0, 0
 }
-
 
 func main() {
-  fmt.Println("Part one:")
-  
-  lines := readFile("input.txt")
-  total := 0
-  direction := complex(0, -1)
-  x,y := startingPosition(lines)
-  var visited map[string]bool
-  visited = make(map[string]bool)
+	fmt.Println("Part one:")
 
-  for {
-    pos := string(x) + string(y)
-    if !visited[pos]{
-      visited[pos] = true
-      total ++
-    } 
+	lines := readFile("input.txt")
+	total := 0
+	direction := complex(0, -1)
+	x, y := startingPosition(lines)
+	var visited map[string]bool
+	visited = make(map[string]bool)
 
-    newX, newY := x + int(real(direction)), y + int(imag(direction))
-    if !inArea(&lines, newX, newY){
-      break
-    }
-    front := string(lines[newY][newX])
-    if front == "#"{
-      direction = direction * complex(0, 1)
-      continue
-    }
-    
-    x,y = newX, newY
-}
+	for {
+		pos := string(x) + string(y)
+		if !visited[pos] {
+			visited[pos] = true
+			total++
+		}
 
-  fmt.Println("Distinct positions: ", total)
+		newX, newY := x+int(real(direction)), y+int(imag(direction))
+		if !inArea(&lines, newX, newY) {
+			break
+		}
+		front := string(lines[newY][newX])
+		if front == "#" {
+			direction = direction * complex(0, 1)
+			continue
+		}
+
+		x, y = newX, newY
+	}
+
+	fmt.Println("Distinct positions: ", total)
 
 }
